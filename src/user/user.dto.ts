a3b01e436ab609700a1a9fd9e4f794fdc6446f71
theHalid <the_halidcastle@mail.ru>
2025-01-10 03:56:57 +0600

    user and auth done

---
+++ src/user/user.dto.ts
@@ -0,0 +1,43 @@
+import {
+	IsOptional,
+	IsNumber,
+	Min,
+	Max,
+	IsString,
+	MinLength,
+	IsEmail
+} from 'class-validator'
+
+export class PomodoroSettingsDto {
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	workInterval?: number
+
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	breakInterval?: number
+
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	@Max(10)
+	intervalCount?: number
+}
+export class UserDto extends PomodoroSettingsDto {
+	@IsEmail()
+	@IsOptional()
+	email: string
+
+	@IsString()
+	@IsOptional()
+	name?: string
+
+	@IsOptional()
+	@MinLength(6, {
+		message: 'Password must be atleast 6 characters long'
+	})
+	@IsString()
+	password: string
+}
